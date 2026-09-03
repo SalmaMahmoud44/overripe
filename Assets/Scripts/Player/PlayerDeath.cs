26a7a11d942c60aa7058ccba2d8f53fff8e73bd3
Maryam ahmed <147929934+maryamahmed0@users.noreply.github.com>
2026-09-03 18:49:44 +0300

    add Jump + Dash + Die Animations

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -1,36 +1,52 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour , IDamagable
 {
     RotTimer rotTimer;
+    Animator animator;
+    bool isDead = false;
     void Start()
     {
         rotTimer = GameObject.Find("RotTimerCanvas").GetComponent<RotTimer>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        Die();
+        if (rotTimer.currentTime <= 0 && !isDead)
+        {
+            Debug.Log("Player has died due to rot timer reaching zero.");
+            Die();
+        }
     }
 
     void Die()
     {
-        if (rotTimer.currentTime <= 0 )
-        {
-            Debug.Log("Player has died due to rot timer reaching zero.");
-            // Add death logic here (e.g., reload scene, show game over screen, etc.)
-            Destroy(gameObject); // Example: Destroy the player object
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload the current scene
-
-        }
+        isDead = true;
+        animator.SetTrigger("isDead");
+        StartCoroutine(RestartLevel());
     }
 
     public new void TakeDamage(float damage)
     {
-        // Implement player damage logic here if needed
         Debug.Log("Player took damage: " + damage);
         rotTimer.AddTime(-damage); // Assuming damage reduces times
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Juice"))
+        {
+           Die();
+        }
+    }
+
+    IEnumerator RestartLevel()
+    {
+        yield return new WaitForSeconds(1.5f); // Wait a few seconds before restarting
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
 }
